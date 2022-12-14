92303659bcff35d7057190f27d08358b15966628
Jon Gill <jon@jonagill.com>
2022-12-13 23:41:44 -0800

    Add MockTimeProvider

---
+++ Packages/com.jonagill.asyncroutines/Unit Tests/MockTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncRoutines
+{
+    public class MockTimeProvider : ITimeProvider
+    {
+        public float Time { get; set; }
+        public float DeltaTime { get; set; }
+        public float FixedDeltaTime { get; set; }
+        public float FrameCount { get; set; }
+        public float RealTimeSinceStartup { get; set; }
+    }    
+}
