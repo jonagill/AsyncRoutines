0fc9d988215fcd8f9906321dd4393b97fab54368
Jon Gill <jon@jonagill.com>
2022-12-13 23:41:18 -0800

    Add play mode and editor time providers

---
+++ Packages/com.jonagill.asyncroutines/Editor/EditorTimeProvider.cs
@@ -0,0 +1,42 @@
+﻿using UnityEditor;
+
+namespace AsyncRoutines
+{
+    public class EditorTimeProvider : ITimeProvider
+    {
+        static EditorTimeProvider()
+        {
+            frameCount = 0;
+            lastUpdateTime = -1f;
+            deltaTime = -1f;
+
+            EditorApplication.update += OnUpdate;
+        }
+        
+        private static int frameCount;
+        private static float lastUpdateTime;
+        private static float deltaTime;
+
+        private static void OnUpdate()
+        {
+            var time = (float) EditorApplication.timeSinceStartup;
+            if (lastUpdateTime > 0f)
+            {
+                deltaTime = time - lastUpdateTime;
+            }
+            else
+            {
+                deltaTime = 0f;
+            }
+            
+            lastUpdateTime = time;
+            frameCount++;
+        }
+
+        public float Time => (float) EditorApplication.timeSinceStartup;
+        public float DeltaTime => deltaTime;
+        public float FixedDeltaTime => deltaTime;
+        public float FrameCount => frameCount;
+        public float RealTimeSinceStartup => (float) EditorApplication.timeSinceStartup;
+    }
+}
---
+++ Packages/com.jonagill.asyncroutines/Runtime/PlayModeTimeProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace AsyncRoutines
+{
+    public class PlayModeTimeProvider : ITimeProvider
+    {
+        public float Time => UnityEngine.Time.time;
+        public float DeltaTime => UnityEngine.Time.deltaTime;
+        public float FixedDeltaTime => UnityEngine.Time.fixedDeltaTime;
+        public float FrameCount => UnityEngine.Time.frameCount;
+        public float RealTimeSinceStartup => UnityEngine.Time.realtimeSinceStartup;
+    }
+}
