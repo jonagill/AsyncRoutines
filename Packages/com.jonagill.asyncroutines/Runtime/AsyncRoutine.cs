69c31643cca36bdef1276de2d52d8065962e35f2
Jon Gill <jon@jonagill.com>
2022-12-14 16:26:29 -0800

    Add helper methods for running routines on the default AsyncRoutineRunner

---
+++ Packages/com.jonagill.asyncroutines/Runtime/AsyncRoutine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsyncRoutines
+{
+    public static class AsyncRoutine
+    {
+        public static IAsyncRoutinePromise RunRoutine(Behaviour behaviour, IEnumerator<IYieldInstruction> routine)
+        {
+            return AsyncRoutineRunner.DefaultRunner.Run(behaviour, routine);
+        }
+        
+        public static IAsyncRoutinePromise RunRoutine(IEnumerator<IYieldInstruction> routine)
+        {
+            return AsyncRoutineRunner.DefaultRunner.Run(null, routine);
+        }
+    }
+}
---
+++ Packages/com.jonagill.asyncroutines/Runtime/Extensions/BehaviourExt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsyncRoutines
+{
+    public static class BehaviourExt
+    {
+        public static IAsyncRoutinePromise RunRoutine(this Behaviour behaviour, IEnumerator<IYieldInstruction> routine)
+        {
+            return AsyncRoutine.RunRoutine(behaviour, routine);
+        }
+    }
+}
